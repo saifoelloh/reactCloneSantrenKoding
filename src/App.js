32532df5f90182419ceb27a9f5fc037b58f9f542
saifoelloh <saifoelloh@gmail.com>
2018-08-01 14:57:58 +0700

    battlefield

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import './custom.css'
+import Aux from './hoc/Auxilary.jsx'
 import NavBar from './component/Navbar.jsx'
 import JumboTron from './component/Jumbotron.jsx'
-import { Container, Row, Col } from 'reactstrap';
-import Aux from './hoc/Auxilary.jsx'
 import KajianKoding from './component/KajianKoding.jsx'
 import SantrenKilat from './component/SantrenKilat.jsx'
+import KegiatanMondok from './component/KegiatanMondok.jsx'
+import Mondok from './component/Mondok.jsx'
+import SponsorPartner from './component/SpoonsorPartner.jsx'
+import Kontak from './component/Kontak.jsx'
+import Footer from './component/Footer.jsx'
 
 class App extends Component {
     render() {
@@ -16,6 +22,15 @@ class App extends Component {
                 <hr />
                 <SantrenKilat />
                 <hr />
+                <KegiatanMondok />
+                <hr />
+                <Mondok />
+                <hr />
+                <SponsorPartner />
+                <hr />
+                <Kontak />
+                <hr />
+                <Footer />
             </Aux>
         )
     }
